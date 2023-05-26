228353abea369e7a72d4d9b340e791a4f58adbf2
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-05-26 21:02:23 +0530

    new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/assets/adidas.png 	new file:   src/assets/bars.png 	new file:   src/assets/calories.png 	new file:   src/assets/dumbell.png 	new file:   src/assets/dumbell.svg 	new file:   src/assets/flame.svg 	new file:   src/assets/github.png 	new file:   src/assets/heart.png 	new file:   src/assets/heartHealth.svg 	new file:   src/assets/hero_image.png 	new file:   src/assets/hero_image_back.png 	new file:   src/assets/image1.png 	new file:   src/assets/image2.png 	new file:   src/assets/image3.png 	new file:   src/assets/image4.png 	new file:   src/assets/instagram.png 	new file:   src/assets/leftArrow.png 	new file:   src/assets/linkedin.png 	new file:   src/assets/logo.png 	new file:   src/assets/nb.png 	new file:   src/assets/nike.png 	new file:   src/assets/noise.png 	new file:   src/assets/rightArrow.png 	new file:   src/assets/runing.png 	new file:   src/assets/runing.svg 	new file:   src/assets/t-image1.png 	new file:   src/assets/t-image2.jpg 	new file:   src/assets/t-image3.jpg 	new file:   src/assets/tick.png 	new file:   src/assets/whiteTick.png 	new file:   src/components/Hero/Header/Header.css 	new file:   src/components/Hero/Header/Header.jsx 	new file:   src/components/Hero/Hero.css 	new file:   src/components/Hero/Hero.jsx 	new file:   src/components/Programs/Programs.css 	new file:   src/components/Programs/Programs.jsx 	new file:   src/components/Reasons/Reasons.css 	new file:   src/components/Reasons/Reasons.jsx 	new file:   src/index.css 	new file:   src/index.js 	new file:   yarn.lock

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './Hero.css'
+import Header from '../Header/Header'
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
+
+export const Hero=()=>{
+    return (
+        <div className="hero">
+            <div className='hero-left'> 
+                <Header/>
+                {/* The best ad */}
+                <div className='the-best-ad'>
+                    <div></div>
+                    <span>the best fitness club in the world</span>
+                </div>
+                {/* Hero Heading */}
+                <div className='hero-text'>
+                    <div>
+                        <span className='stroke-text'>Shape</span>
+                        <span>Your</span>
+                    </div>
+                    <div><span>Ideal Body</span></div>
+                    <div><span>In here we will help you to shape and buiild your ideal body</span></div>
+                </div>
+                {/* experiance figures */}
+                <div className="figures">
+                    <div><span>+140</span><span>expert coachs</span></div>
+                    <div><span>+978</span><span>members joined</span></div>
+                    <div><span>+50</span><span>fitness programs</span></div>
+                </div>
+                {/* Hero buttons */}
+                <div className="hero-buttons">
+                    <buttons className="btn">Get Started</buttons>
+                    <buttons className="btn">Learn More</buttons>
+                </div>
+            </div>
+            <div className='hero-right'>
+                <button className="btn">Join Now</button>
+                <div className="heart-rate">
+                    <img src={Heart} alt="" />
+                    <span>Heart Rate</span>
+                    <span>116 BPM</span>
+                </div>
+                {/* Hero images */}
+                <img src={hero_image} alt="" className='hero-image' />
+                <img src={hero_image_back} alt="" className='hero-image-back' />
+                {/* Calories */}
+                <div className="calories">
+                    <img src={Calories} alt="" />
+                    <div>
+                        <span>Calories Burned</span>
+                    <span>220 kcal</span>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    )
+}
