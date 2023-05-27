078a18c35edca00eb8e8e459d90da9f87ca6bb34
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-05-27 18:22:48 +0530

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Footer/Footer.css 	new file:   src/components/Footer/Footer.jsx 	modified:   src/components/Hero/Hero.css 	modified:   src/components/Hero/Hero.jsx 	new file:   src/components/Join/Join.css 	new file:   src/components/Join/Join.jsx 	modified:   src/components/Testimonials/Testimonials.css

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Join.css'
+
+const Join=()=>{
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className='stronk-text'> READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className='stronk-text'> WITH US?</span>
+        </div>
+      </div>
+
+      <div className="right-j">
+        <form  className="email-container" >
+          <input type="email" name="user_email" placeholder='Enter you email address' />
+          <button className='btn btn-j'>Join Now</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Join
